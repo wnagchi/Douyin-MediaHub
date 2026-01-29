9c6737f7f4339561f48464d01fe03d78d726b562
一心诗意喂了狗 <1305bcd@gmail.com>
2026-01-29 18:00:12 +0800

    fix: update revision for precached index.html in service worker

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.6sta45v20cg"
+    "revision": "0.arap86mrkt8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
