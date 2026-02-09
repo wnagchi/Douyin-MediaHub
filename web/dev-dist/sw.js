bca601167c37e0198a3b7befc1ef0dc5bd1eacf0
一心诗意喂了狗 <1305bcd@gmail.com>
2026-02-09 16:29:42 +0800

    fix: update index.html revision in service worker and adjust modal video max-height and background gradient in CSS

--- web/dev-dist/sw.js
+++ web/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.e1m8rcjpu4g"
+    "revision": "0.u5chbp2abs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
